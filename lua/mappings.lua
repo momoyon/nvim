ab28ca4074369f14e35e84d4ca8692a9c18029f2
ahmedsamyh <ahmedsamyh10@gmail.com>
2026-05-01 17:13:11 +0500

    [mappings.lua] gitsigns next and prev hunk

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,10 +6,12 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<leader>ww", ":set wrap!<CR>", { desc = "Toggle line wrap"})
+map("n", "<leader>ww", ":set wrap!<CR>", { desc = "Toggle line wrap" })
 
 -- GitSigns mappings
-map("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview gitsigns hunk line"})
-map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk"})
+map("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview gitsigns hunk line" })
+map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+map("n", "]g", ":Gitsigns next_hunk<CR>", { desc = "Goes to the next git hunk" })
+map("n", "[g", ":Gitsigns prev_hunk<CR>", { desc = "Goes to the prev git hunk" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
